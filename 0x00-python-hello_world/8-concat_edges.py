55dd7caf397aab8c085b978ee9964232c324ec35
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-06-05 15:07:04 +0300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntaxob"
-str = str[39:67] + str[106:111] + " " + str[0:6]
+str = str[39:67] + str[106:111].strip() + " " + str() + str[0:6]
 print(str)
