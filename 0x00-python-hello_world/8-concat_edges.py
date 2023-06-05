d7bd40cba1b3c433cf09f0cf727371f708e0adc6
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-06-05 13:26:13 +0300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str2 = "object-oriented programming with Python"
-print(str2)
+str = "object-oriented programming with Python"
+print(str)
