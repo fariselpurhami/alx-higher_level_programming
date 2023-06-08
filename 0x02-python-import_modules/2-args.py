51b0527e5847e9ec7739e1da646d28a94939ccfb
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-06-08 15:58:10 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     if len(argv) == 2:
         print('{} arguments:'.format(len(argv) - 1))
-        print('{}: {}'.format(1, argv.__getitem__(1)))\
+        print('{}: {}'.format(1, argv.__getitem__(1)))
 
     elif len(argv) > 2:
         print('{} arguments:'.format(len(argv) - 1))
