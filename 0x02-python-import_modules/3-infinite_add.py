776155bfc4ea5f401e6d2ece72a2dc9667a58fe3
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-08 06:57:08 -0700

    updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     sum = len(argv)
     add = 0
-  
+
     if sum == 1:
         print("{}".format(add))
-      
+
     else:
         for w in range(1, sum):
             add += int(argv.__getitem__(w))
