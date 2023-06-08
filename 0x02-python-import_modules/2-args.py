ccda89eda0640026956a01d7e757445f29e3d000
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-08 05:55:58 -0700

    updated

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+
+    if len(argv) == 2:
+        print('{} arguments:'.format(len(argv) - 1))
+        print('{}: {}'.format(1, argv.__getitem__(1)))\
+
+    elif len(argv) > 2:
+        print('{} arguments:'.format(len(argv) - 1))
+
+        for w in range(1, len(argv)):
+            print('{}: {}'.format(w, argv.__getitem__(w)))
+
+    else:
+        print('{} arguments.'.format(len(argv) - 1))
