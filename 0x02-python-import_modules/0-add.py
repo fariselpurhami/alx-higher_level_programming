427d1473aa853f0c2071d5557a6a2281f26170f2
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-06-08 12:06:48 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-from add_0 import add
-if __name__ == '__main__':
-    a = 1 
-    b = 2
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-=======
 #!/usr/bin/python3
 from add_0 import add
     a = 1 
     b = 2
     print('{} + {} = {}'.format(a, b, add(a, b)))
->>>>>>> a218e1416554a4fddecf7385d21527e8157ec892
