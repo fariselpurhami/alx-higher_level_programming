371a24dcf36fab367712451318813064e4b5ad2b
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-09 03:04:28 -0700

    updated

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    if matrix == None:
+        print('')
+
+    else:
+        for w in range(len(matrix)):
+
+            for m in range(len(matrix[w])):
+                print('{}'.format(matrix[w][m]), end='')
+
+                if m != len(matrix[w]) - 1:
+                    print(' ', end='')
+
+            print('')
