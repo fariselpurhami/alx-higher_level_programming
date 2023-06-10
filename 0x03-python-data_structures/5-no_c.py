17edc4bdeddefb7e6bfcf0e7e09a0bd5084fb728
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-10 02:39:45 -0700

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
 
     new_str = ''
     for w in range(len(my_string)):
 
-        if my_string[w] == 'C' or my_string[w] == 'c':
+        if my_string[w] == 'c' or my_string[w] == 'C':
             pass
 
         else:
