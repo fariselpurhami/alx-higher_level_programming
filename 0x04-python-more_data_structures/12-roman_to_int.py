0ae77f9187af818cafe26b221e21d61e5967d187
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-14 02:38:48 -0700

    updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,19 +10,17 @@ def roman_to_int(roman_string):
             'D': 500,
             'M': 1000
     }
-
     i = 0
     z = 0
 
     if type(roman_string) is str and roman_string:
 
         for e in range(len(roman_string) - 1, -1, -1):
-            if x[roman_string[e]] >= z:
 
+            if x[roman_string[e]] >= z:
                 i += x[roman_string[e]]
             else:
                 i -= x[roman_string[e]]
-
             z = x[roman_string[e]]
 
         return (i)
