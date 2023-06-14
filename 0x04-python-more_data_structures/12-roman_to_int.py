484cf19ed93198a98154fd90293ae7d1d26dfb7d
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-14 04:01:13 -0700

    updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,13 @@
 def roman_to_int(roman_string):
 
     x = {
-            'I': (1),
-            'V': (5),
-            'X': (10),
-            'L': (50),
-            'C': (100),
-            'D': (500),
-            'M': (1000)
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
     }
 
     n = (0)
@@ -18,12 +18,12 @@ def roman_to_int(roman_string):
 
         for e in range(len(roman_string) - 1, -1, -1):
 
-            if x[roman_string[e]] >= z:
-                n += x[roman_string[e]]
+            if (x[roman_string[e]] >= z):
+                n += (x[roman_string[e]])
 
             else:
 
-                n -= x[roman_string[e]]
-            z = x[roman_string[e]]
+                n -= (x[roman_string[e]])
+            z = (x[roman_string[e]])
 
         return (n)
