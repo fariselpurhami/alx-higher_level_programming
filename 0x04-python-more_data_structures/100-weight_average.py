5de156ba2264c9842b9821f28c7aaef8df5cfd47
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-14 07:08:08 -0700

    updated

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+
+    if not my_list:
+
+        return (0)
+
+    else:
+
+        a = 0
+        v = 0
+
+        for c in my_list:
+
+            a += (c[1])
+            v = (c[0]) = c[1])
+
+    return (a / v)
