9618405d4951a239106fdca56240e4f9fdcdea16
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-14 01:13:00 -0700

    updated

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
 
-    extra = set(my_list)
-
-    num = 0
-    for worth in extra:
-
-        num += worth
-
-    return (num)
+    return sum(set(my_list))
